d2857cfe8205e1c51c0f50799e2c6e646c0ff0f5
GH-Swapna22 <swapnaforjobs@gmail.com>
2026-05-16 08:00:26 +0000

    commit1

---
+++ airbnb_project/models/src/src_reviews.sql
@@ -0,0 +1,15 @@
+
+WITH result_set AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.RAW.RAW_REVIEWS
+)
+SELECT
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments AS review_text,
+    sentiment AS review_sentiment
+FROM
+    result_set
